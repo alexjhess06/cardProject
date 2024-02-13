fbd266fb4dd0c2b62b9cb91f9f984c7c5f68c5f6
alexander.hess <alexander.hess@MU-C1-08.mullen.pvt.k12.co.us>
2024-02-13 08:56:02 -0700

    Initial commit

---
+++ Card Check/src/cardCheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class cardCheck
+	{
+		static Scanner userInput = new Scanner(System.in); 
+		static Long[] cardNum = new Long[16];
+		public static void main(String[] args)
+			{
+				 
+				 System.out.println("Enter a credit/debit card number, AMEX not accepted");
+				 //long card = userInput.nextLong();
+				
+				
+				 getNum();
+			}
+
+		private static void getNum()
+			{
+			long card = userInput.nextLong();
+			for(int i = 0; i <= 16; i++) {
+				
+			}
+				
+			}
+
+	}
