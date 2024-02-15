ae88e37310111aa243e1d1bf08e1b93ee228affc
alexander.hess <alexander.hess@MU-C1-08.mullen.pvt.k12.co.us>
2024-02-15 09:18:57 -0700

    Completed card check, most of the way through random cards

--- Card Check/src/cardCheck.java
+++ Card Check/src/cardCheck.java
@@ -1,18 +1,48 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.Math;
 public class cardCheck
 	{
 		static Scanner userInput = new Scanner(System.in); 
 		static Long[] cardNum = new Long[16];
+		static int[] randCardNum = new int[16];
+		static String[] randNums = new String[100];
+		static long total = 0;
+		static int giveGet = 0;
+		static int valid = 0;
+		static boolean checkValidity = false;
 		public static void main(String[] args)
 			{
-				 
+				 System.out.println("Would you like to (1)generate random card numbers or (2)check a card number?");
+				 giveGet = userInput.nextInt();
+				 if(giveGet == 2) {
 				 System.out.println("Enter a credit/debit card number, AMEX not accepted");
-				 //long card = userInput.nextLong();
-				
-				
 				 getNum();
 				 doMath();
+				 isValid();
+				 }
+				if(giveGet==1) {
+					checkValidity = true;
+					while(checkValidity == true) {
+					genNums();
+					doMath();
+					isValid();
+					}
+				}
+				
+				
+				 
+			}
+
+		private static void genNums()
+			{
+				
+			for(int i = 0; i < 16; i++) {
+				int rand = (int)(Math.random() * 10);
+				randCardNum[i] = rand;
+				randCardNum.toString();
+			}
+				//System.out.println(Arrays.toString(randCardNum));
 			}
 
 		private static void getNum()
@@ -28,6 +58,8 @@ public class cardCheck
 			}
 		private static void doMath()
 		{
+			//----------------------------------------GIVEN CARDS---------------------------------------------//
+			if(giveGet == 2) {
 			for(int i = 0; i < 16; i+=2) {
 				cardNum[i] *= 2;
 			
@@ -43,6 +75,52 @@ public class cardCheck
 			}
 			}
 			System.out.println(Arrays.toString(cardNum));
+			for(int i = 0; i < 16; i++) {
+				total = total + cardNum[i];
+			}
+			System.out.println(total);
+			}
+			//------------------------------------------RAND CARDS--------------------------------------------------------//
+			if(giveGet == 1) {
+				for(int i = 0; i < 16; i+=2) {
+					randCardNum[i] *= 2;
+				
+				//System.out.println(Arrays.toString(randCardNum));
+				for(int digit : randCardNum) {
+					String digitStr = String.valueOf(digit);
+					int length = digitStr.length();
+					if(length == 1) {
+						int a = randCardNum[i] / 10;
+						int b = randCardNum[i] % 10;
+						randCardNum[i] = a+b;
+					}
+				}
+				}
+				//System.out.println(Arrays.toString(randCardNum));
+				for(int i = 0; i < 16; i++) {
+					total = total + randCardNum[i];
+				}
+				System.out.println(total);
+				total = 0;
+			}
+		}
+		private static void isValid()
+		{
+			if(total % 10 == 0) {
+				System.out.println("This is possibly a valid credit card number!");
+					valid++;
+					for(int i : randCardNum) {
+						 String str = randCardNum.toString();
+						 randNums[i] = str;
+						}
+				if(valid == 100) {
+					checkValidity = false;
+					System.out.println("Here is your 100 random potentially valid credit card numbers! \n" + randNums);
+				}
+			}
+			else {
+				System.out.println("This credit card number is invalid!");
+			}
 		}
 
 	}
